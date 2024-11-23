ad374b216485b8c564333f9b5a405b7e5ec0dcde
MatthewAraujo <matthewaraujo20@gmail.com>
2024-11-23 19:45:17 -0300

    foudase

--- src/main/java/com/empresa/projetoapi/database/Database.java
+++ src/main/java/com/empresa/projetoapi/database/Database.java
@@ -3,15 +3,29 @@ package com.empresa.projetoapi.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class Database {
-    private static final String URL = "jdbc:postgresql://junction.proxy.rlwy.net:46588/empresa_java";
-    private static final String USER = "docker";
-    private static final String PASSWORD = "docker";
+    private static final Dotenv dotenv = Dotenv.configure().directory("./").load();
+
+    private static final String URL = dotenv.get("DATABASE_URL") + "?ssl=true";
+    private static final String USER = dotenv.get("DATABASE_USER");
+    private static final String PASSWORD = dotenv.get("DATABASE_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         try {
-            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Tentando conectar ao banco...");
+            System.out.println("URL: " + URL);
+            System.out.println("Usuário: " + USER);
+
+            Properties props = new Properties();
+            props.setProperty("user", USER);
+            props.setProperty("password", PASSWORD);
+            props.setProperty("ssl", "true");
+
+            Connection connection = DriverManager.getConnection(URL, props);
             System.out.println("Conexão bem-sucedida!");
             return connection;
         } catch (SQLException e) {
