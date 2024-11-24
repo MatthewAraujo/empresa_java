2194b472842e56604bc4de8202b509bceb307497
MatthewAraujo <matthewaraujo20@gmail.com>
2024-11-24 16:24:07 -0300

    certo demaist

--- src/main/java/com/empresa/projetoapi/database/Database.java
+++ src/main/java/com/empresa/projetoapi/database/Database.java
@@ -3,7 +3,6 @@ package com.empresa.projetoapi.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 import io.github.cdimascio.dotenv.Dotenv;
 
@@ -14,24 +13,17 @@ public class Database {
     private static final String USER = dotenv.get("DATABASE_USER");
     private static final String PASSWORD = dotenv.get("DATABASE_PASSWORD");
 
+
+
     public static Connection getConnection() throws SQLException {
         try {
-            System.out.println("Tentando conectar ao banco...");
-            System.out.println("URL: " + URL);
-            System.out.println("Usuário: " + USER);
-
-            Properties props = new Properties();
-            props.setProperty("user", USER);
-            props.setProperty("password", PASSWORD);
-            props.setProperty("ssl", "true");
-
-            Connection connection = DriverManager.getConnection(URL, props);
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Conexão bem-sucedida!");
             return connection;
         } catch (SQLException e) {
             System.out.println("Erro ao conectar: " + e.getMessage());
+            e.printStackTrace();
             throw e;
         }
     }
 }
-
